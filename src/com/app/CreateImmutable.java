cb1bc26b213d06458b74f53e1253710a38710b74
Dinesh <Dinesh@DESKTOP-22F7E5S>
2018-12-31 15:11:36 +0530

    ImmutableApp

---
+++ src/com/app/CreateImmutable.java
@@ -0,0 +1,25 @@
+package com.app;
+
+final  class CreateImmutable {
+	private int i;
+
+	public CreateImmutable(int i) {
+		super();
+		this.i = i;
+	}
+	
+	public CreateImmutable modify(int i){
+		if(this.i==i){
+			return this;
+		}else{
+			return new CreateImmutable(i);
+		}
+	}
+	public static void main(String[] args) {
+		
+		CreateImmutable c1= new CreateImmutable(100);
+		CreateImmutable c2= c1.modify(10);
+		CreateImmutable c3= c1.modify(100);
+		System.out.println(c1.equals(c3));
+	}
+}
